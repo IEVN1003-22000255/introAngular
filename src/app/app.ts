df05040915fb2a5463f642b08f0347ddb0e7d940
Andrestapia <82144@alumnos.utleon.edu.mx>
2026-09-10 21:08:16 -0600

    heroes 10/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,17 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('introAngular');
+
+  title: string = 'hernandez tapia oscar';
+
+  duplicarNumero(a: number): number {
+    return a * 2;
+  }
+
+  pelicula = {
+    titulo: 'spider man',
+    fechaLanzamiento: new Date(),
+    precio: 2535
+  };
 }
+
